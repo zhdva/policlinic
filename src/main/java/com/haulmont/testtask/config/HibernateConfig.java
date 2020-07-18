8aa14846e009b4d968818a8218686bc27f34bfe2
Alexander Zhadaev <a.v.zhadaev@gmail.com>
2020-07-18 11:43:00 +0400

    added Hibernate

---
+++ src/main/java/com/haulmont/testtask/config/HibernateConfig.java
@@ -0,0 +1,33 @@
+package com.haulmont.testtask.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import com.haulmont.testtask.model.*;
+
+public class HibernateConfig {
+
+    private static SessionFactory sessionFactory;
+
+    private HibernateConfig() {}
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+
+            configuration.addAnnotatedClass(Doctor.class);
+            configuration.addAnnotatedClass(Patient.class);
+            configuration.addAnnotatedClass(Prescription.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+
+        }
+
+        return sessionFactory;
+    }
+
+}
